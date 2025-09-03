7e46ca3fddc07308f44577ef1e5a6f1470d92d43
Patrick Popores <patrick@larderlogic.com>
2025-09-03 12:49:50 -0400

    first commit

---
+++ forms/WTForm.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+
+class EditorForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    content = TextAreaField("Content", validators=[DataRequired()])
